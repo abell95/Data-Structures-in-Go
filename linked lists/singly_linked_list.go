3268f6104df373780adbb30c567dd7c518c8be65
abell95 <andrewfentonbell@gmail.com>
2018-02-13 17:58:35 -0800

    improvements made to skeleton functions but broke a few

--- linked lists/singly_linked_list.go
+++ linked lists/singly_linked_list.go
@@ -1,4 +1,4 @@
-//Needs constructor method, search method and delete method
+//Needs search method and delete method
 
 package main
 
@@ -15,54 +15,79 @@ type Link struct {
 // List points to head of list
 type List struct {
 	first *Link
+	size  int
 }
 
-func (l List) appendLink(num int) {
+func createEmptyList() *List {
+	newList := new(List)
+	newList.first = nil
+	return newList
+}
+
+func createListContaining(args ...int) *List {
+	newList := createEmptyList()
+	for _, n := range args {
+		newList.appendLink(n)
+	}
+	return newList
+}
+
+func (l *List) appendLink(num int) {
 	newLink := new(Link)
 	newLink.val, newLink.next = num, nil
 
 	iter := l.first
 	if iter == nil {
 		l.first = newLink
+		return
 	}
 	for iter.next != nil {
 		iter = iter.next
 	}
 	iter.next = newLink
+	l.size++
 }
 
-func (l List) appendMultipleLinks(args ...int) {
-	//Do the thing it says
-	for x := range args {
+func (l *List) appendMultipleLinks(args ...int) {
+	for _, x := range args {
 		l.appendLink(x)
+		l.size++
 	}
 }
 
-func (l List) prependLink(num int) {
-	//add link to start of list
+//add link to start of list, push rest down
+func (l List) prependLink(num int) { // broken
+	newLink := new(Link)
+	newLink.val = num
+	temp := l.first
+	l.first = newLink
+	newLink.next = temp
 }
 
-func (l List) removeFromList(remove int) {
-	iter := l.first
-	if iter == nil {
-		fmt.Println("List is empty")
-		return
-	} else if iter.val == remove {
-		l.first = nil
-	}
-	for iter.next.val != remove {
-		iter = iter.next
-	}
-	iter.next = iter.next.next
-}
+// func (l List) prependMultipleLinks(args ...int) {
+// 	for _, x := range args {
+// 		//prependLink(x)
+// 		x++
+// 		l.size++
+// 	}
+// }
 
-func (l List) countListSize() int {
-	ctr := 0
-	iter := l.first
-	if iter == nil {
-		return ctr
-	}
-	return ctr
+// func (l *List) removeFromList(remove int) {
+// 	iter := l.first
+// 	if iter == nil {
+// 		fmt.Println("List is empty")
+// 		return
+// 	} else if iter.val == remove {
+// 		l.first = nil
+// 	}
+// 	for iter.next.val != remove {
+// 		iter = iter.next
+// 	}
+// 	iter.next = iter.next.next
+// }
+
+func (l List) getListSize() int { //this needs work
+	return l.size
 }
 
 //fill and print slice containing values of list
@@ -83,21 +108,33 @@ func (l List) printList() {
 
 func (l List) printFromXToY(x, y int) {
 	printer := l.first
-	if printer == nil {
-		fmt.Println("List empty")
-		return
+	for i := 0; i < x; i++ {
+		if printer.next == nil {
+			fmt.Println("Outside of list range")
+			return
+		}
+		printer = printer.next
 	}
-	for printer != nil {
+	//s := []int{}
 
-	}
 }
 
+// func (l List) numIsInList() (bool, int) {
+// 	return false, 0
+// }
+
 func main() {
-	listLink := Link{10, nil}
-	linkList := List{&listLink}
+	linkList := createEmptyList()
 	linkList.appendLink(5)
 	linkList.appendLink(7)
 	linkList.appendLink(9)
-	linkList.removeFromList(5)
+	linkList.prependLink(12)
 	linkList.printList()
+	// linkList.removeFromList(5)
+	linkList.appendMultipleLinks(1, 2, 3, 4, 5)
+	linkList.printList()
+	fmt.Println(linkList.getListSize())
+
+	anotherList := createEmptyList()
+	anotherList.printList()
 }
